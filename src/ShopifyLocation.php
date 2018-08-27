c8b362a6d16ac4c6a7b30fe9ce65fa376099131c
Kanazawaa <ayazarashi@gmail.com>
2018-08-27 13:40:44 +0900

    location追加、create fulfillmentでレスポンスをreturn

--- src/ShopifyLocation.php
+++ src/ShopifyLocation.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @copyright Copyright (c) 2017 Shopify Inc.
- * @license MIT
- */
 
 namespace Shopify;
 
@@ -13,4 +9,11 @@ class ShopifyLocation extends ShopifyObject
 
     const PLURAL = "locations";
     const SINGULAR = "location";
+
+    public function readLocationLevels($id)
+    {
+        $resource = static::PLURAL . DIRECTORY_SEPARATOR . $id . DIRECTORY_SEPARATOR .'inventory_levels';
+        return $this->client->call("GET", $resource, null, []);
+    }
+
 }
